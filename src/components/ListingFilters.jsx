9e34490a99cb47cdc1b5f4893f56ab0939f76c15
rnldvrgl <delacruz.ronaldvergel@gmail.com>
2025-02-17 14:23:37 +0800

    chore: add state and event handles to listing filters

--- src/components/ListingFilters.jsx
+++ src/components/ListingFilters.jsx
@@ -1,15 +1,20 @@
 import { Button, DateRangePicker, Input, Stepper } from "@/components/ui"
 import { Search } from 'lucide-react'
+import { useState } from "react"
 const ListingFilters = () => {
+    const [dates, setDates] = useState()
+    const [guests, setGuests] = useState(0)
+    const [search, setSearch] = useState('')
+    
   return (
   <div className='flex flex-row items-center justify-center gap-2'>
-    <Input className='w-[400px]' placeholder='Search destinations'/>
-    <DateRangePicker placeholder='Add dates' />
-    <Stepper/>
+    <Input className='w-[400px]' placeholder='Search destinations'value={search} onChange={(e) => setSearch(e.target.value)}/>
+    <DateRangePicker placeholder='Add dates' value={dates} onChange={setDates} minDate={new Date()}/>
+    <Stepper label='guest' value={guests} onChange={setGuests}/>
     <Button>
         <Search className ='size-4' />
     </Button>
-  </div>
+    </div>
   )
 }
 

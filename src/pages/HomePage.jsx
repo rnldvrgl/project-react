ca5e439aec7f4b163eb6fe333d117bd0c7060a41
rnldvrgl <delacruz.ronaldvergel@gmail.com>
2025-02-17 14:49:56 +0800

    chore: create callback to update the filters in the homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,16 +1,47 @@
 'use client'
 
-import { listings as statisListings } from '@/api/data/listings';
+import { isListingAvailable, listings as statisListings } from '@/api/data/listings';
 import ListingFilters from '@/components/ListingFilters';
 import ListingList from '@/components/ListingList';
 import { Separator } from '@/components/ui';
 import { useState } from 'react';
 const HomePage = () => {
   const [listings, setListings] = useState(statisListings)
+   
+   const handleFilters = (filters) => {
+    const { dates, guests, search } = filters;
+
+    // Resets filters by using static listings
+    let filteredListings = staticListings;
+
+    // Handles date range
+    if (dates) {
+      filteredListings = filteredListings.filter((listing) =>
+        isListingAvailable(listing, dates),
+      );
+    }
+
+    // Handles guests
+    if (guests) {
+      filteredListings = filteredListings.filter(
+        (listing) => guests <= listing.maxGuests,
+      );
+    }
+
+    // Handles search
+    if (search) {
+      filteredListings = filteredListings.filter((listing) =>
+        listing.name.toLowerCase().includes(search.toLowerCase()),
+      );
+    }
+
+    setListings(filteredListings);
+  };
+  
    return (
      <div className='container py-4'>
       <div className='mb-4'>
-        <ListingFilters/>
+        <ListingFilters onChange={handleFilters}/>
         <Separator className='my-4' />
       </div>
         <ListingList listings={listings} />

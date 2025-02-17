9fe827b05248e2044a2bb72e9f4388a2e2bad23d
rnldvrgl <delacruz.ronaldvergel@gmail.com>
2025-02-17 14:25:17 +0800

    chore: add callback prop to listing filters

--- src/components/ListingFilters.jsx
+++ src/components/ListingFilters.jsx
@@ -5,13 +5,17 @@ const ListingFilters = () => {
     const [dates, setDates] = useState()
     const [guests, setGuests] = useState(0)
     const [search, setSearch] = useState('')
+
+    const handleSubmit = () => {
+        onChange({dates, guests, search});
+    }
     
   return (
   <div className='flex flex-row items-center justify-center gap-2'>
     <Input className='w-[400px]' placeholder='Search destinations'value={search} onChange={(e) => setSearch(e.target.value)}/>
     <DateRangePicker placeholder='Add dates' value={dates} onChange={setDates} minDate={new Date()}/>
     <Stepper label='guest' value={guests} onChange={setGuests}/>
-    <Button>
+    <Button onClick={handleSubmit}>
         <Search className ='size-4' />
     </Button>
     </div>

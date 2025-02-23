dd9f3ad722d71517a2ec712777ec8d9a193c113c
rnldvrgl <delacruz.ronaldvergel@gmail.com>
2025-02-23 12:52:20 +0800

    chore: update ListingCard with new data

--- src/components/ListingCard.jsx
+++ src/components/ListingCard.jsx
@@ -1,5 +1,6 @@
 import { Card, CardContent } from '@/components/ui';
 import { getImageUrl } from '@/lib/utils/images';
+import { DollarSign, Pin, Users } from 'lucide-react';
 
 const ListingCard = ({ listing }) => {
   return (
@@ -9,8 +10,25 @@ const ListingCard = ({ listing }) => {
         src={getImageUrl(listing.images[0])}
         alt={listing.name}
       />
-      <CardContent className='p-4'>
-        <h2 className='mb-0 text-xl font-semibold'>{listing.name}</h2>
+      <CardContent className='flex flex-col gap-2 p-4'>
+        <h2 className='mb-2 text-xl font-semibold'>{listing.name}</h2>
+        <div className='flex items-center gap-2'>
+          <DollarSign className='w-4 h-4 text-primary' />
+          <span className='text-muted-foreground'>
+            <span className='font-bold text-foreground'>{listing.price}</span> /
+            night
+          </span>
+        </div>
+        <div className='flex items-center gap-2'>
+          <Pin className='w-4 h-4 text-primary' />
+          <span className='text-muted-foreground'>{listing.location.name}</span>
+        </div>
+        <div className='flex items-center gap-2'>
+          <Users className='w-4 h-4 text-primary' />
+          <span className='text-muted-foreground'>
+            {listing.maxGuests} Guests
+          </span>
+        </div>
       </CardContent>
     </Card>
   );

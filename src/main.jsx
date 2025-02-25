3dca78a164866c22b8e44a339bb6f12b1bc63a96
rnldvrgl <delacruz.ronaldvergel@gmail.com>
2025-02-25 16:01:21 +0800

    chore: replace App with Router

--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,15 @@ import ReactDOM from 'react-dom/client';
 import { seedLocalDatabase } from '@/api/data/seed';
 import ThemeProvider from '@/components/ThemeProvider';
 
-import App from './App';
 
 import './index.css';
+import Router from './Router';
 
 // DO NOT REMOVE: Seeds the local storage database with data
 seedLocalDatabase();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <ThemeProvider>
-    <App />
+  <Router />
   </ThemeProvider>,
 );

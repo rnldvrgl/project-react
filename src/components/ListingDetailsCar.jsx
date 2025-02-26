17f26885add3f289a0b6b858e60d437aeabb415b
rnldvrgl <delacruz.ronaldvergel@gmail.com>
2025-02-26 08:43:46 +0800

    chore: create the ListingDetailsCard component

---
+++ src/components/ListingDetailsCar.jsx
@@ -0,0 +1,34 @@
+import { DollarSign, Pin, Users } from 'lucide-react';
+
+import { Card, Separator } from '@/components/ui';
+
+const ListingDetailsCard = ({ listing }) => {
+  return (
+    <Card className='p-4 mx-auto'>
+      <div className='flex flex-col gap-2'>
+        <h1 className='mb-2 text-2xl font-bold'>{listing.name}</h1>
+        <div className='flex items-center gap-2'>
+          <DollarSign className='w-4 h-4 text-primary' />
+          <span className='text-muted-foreground'>
+            <span className='font-bold text-foreground'>{listing.price}</span> /
+            night
+          </span>
+        </div>
+        <div className='flex items-center gap-2'>
+          <Pin className='w-4 h-4 text-primary' />
+          <span className='text-muted-foreground'>{listing.location.name}</span>
+        </div>
+        <div className='flex items-center gap-2'>
+          <Users className='w-4 h-4 text-primary' />
+          <span className='text-muted-foreground'>
+            {listing.maxGuests} Guests
+          </span>
+        </div>
+      </div>
+      <Separator className='my-4' />
+      <div className='whitespace-pre-line'>{listing.description}</div>
+    </Card>
+  );
+};
+
+export default ListingDetailsCard;

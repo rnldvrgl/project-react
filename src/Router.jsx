752928fd49726dc49844bf54562dd28e405cb779
rnldvrgl <delacruz.ronaldvergel@gmail.com>
2025-02-26 15:07:26 +0800

    chore: add a details link to ListingCard

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
 import HomePage from "./pages/HomePage";
+import ListingDetailsPage from "./pages/ListingDetailsPage";
 
 const router = createBrowserRouter([
     {
--- src/components/ListingCard.jsx
+++ src/components/ListingCard.jsx
@@ -1,32 +1,35 @@
 import { Card, CardContent } from '@/components/ui';
 import { DollarSign, Pin, Users } from 'lucide-react';
+import { Link } from 'react-router-dom';
 import ListingCardImages from './ListingCardImages';
 
 const ListingCard = ({ listing }) => {
   return (
-    <Card className='w-[320px]'>
-     <ListingCardImages listing={listing} />
-      <CardContent className='flex flex-col gap-2 p-4'>
-        <h2 className='mb-2 text-xl font-semibold'>{listing.name}</h2>
-        <div className='flex items-center gap-2'>
-          <DollarSign className='w-4 h-4 text-primary' />
-          <span className='text-muted-foreground'>
-            <span className='font-bold text-foreground'>{listing.price}</span> /
-            night
-          </span>
-        </div>
-        <div className='flex items-center gap-2'>
-          <Pin className='w-4 h-4 text-primary' />
-          <span className='text-muted-foreground'>{listing.location.name}</span>
-        </div>
-        <div className='flex items-center gap-2'>
-          <Users className='w-4 h-4 text-primary' />
-          <span className='text-muted-foreground'>
-            {listing.maxGuests} Guests
-          </span>
-        </div>
-      </CardContent>
-    </Card>
+    <Link to={`/listings/${listing.id}`}>
+      <Card className='w-[320px]'>
+      <ListingCardImages listing={listing} />
+        <CardContent className='flex flex-col gap-2 p-4'>
+          <h2 className='mb-2 text-xl font-semibold'>{listing.name}</h2>
+          <div className='flex items-center gap-2'>
+            <DollarSign className='w-4 h-4 text-primary' />
+            <span className='text-muted-foreground'>
+              <span className='font-bold text-foreground'>{listing.price}</span> /
+              night
+            </span>
+          </div>
+          <div className='flex items-center gap-2'>
+            <Pin className='w-4 h-4 text-primary' />
+            <span className='text-muted-foreground'>{listing.location.name}</span>
+          </div>
+          <div className='flex items-center gap-2'>
+            <Users className='w-4 h-4 text-primary' />
+            <span className='text-muted-foreground'>
+              {listing.maxGuests} Guests
+            </span>
+          </div>
+        </CardContent>
+      </Card>
+    </Link>
   );
 };
 

9c6df4547385967d4461d90dac39318738614bd9
rnldvrgl <delacruz.ronaldvergel@gmail.com>
2025-02-26 08:50:35 +0800

    chore: add ListingDetailsPage to Router

--- src/Router.jsx
+++ src/Router.jsx
@@ -10,6 +10,10 @@ const router = createBrowserRouter([
             {
                 path: "/",
                 element: <HomePage />
+            },
+            {
+                path: 'listings/:listingId',
+                element: <ListingDetailsPage />
             }
         ]
     }

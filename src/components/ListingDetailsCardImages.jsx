6e0f601b6a57e54b2c99c33036e66faead92ff73
rnldvrgl <delacruz.ronaldvergel@gmail.com>
2025-02-26 15:08:12 +0800

    chore: create the ListingDetailsCardImages component

---
+++ src/components/ListingDetailsCardImages.jsx
@@ -0,0 +1,46 @@
+import { useState } from 'react';
+
+import {
+    Carousel,
+    CarouselContent,
+    CarouselItem,
+    CarouselNext,
+    CarouselPrevious,
+} from '@/components/ui';
+import { getImageUrl } from '@/lib/utils/images';
+
+const ListingDetailsCardImages = ({ listing }) => {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  return (
+    <>
+      <img
+        className='mb-4 h-[500px] w-full rounded-md object-cover'
+        src={getImageUrl(listing.images[currentImageIndex])}
+        alt={listing.name}
+      />
+      <Carousel className='mx-auto mb-4 w-[90%]'>
+        <CarouselContent>
+          {listing.images.map((image, index) => (
+            <CarouselItem
+              key={image}
+              className='cursor-pointer basis-1/3'
+              onClick={() => setCurrentImageIndex(index)}
+              isSelected={index === currentImageIndex}
+            >
+              <img
+                className='object-cover w-full shadow-sm h-52'
+                src={getImageUrl(image)}
+                alt={listing.name}
+              />
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+    </>
+  );
+};
+
+export default ListingDetailsCardImages;

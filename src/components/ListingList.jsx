106a2fc5ab976213bd28af7ba39d2ac51273123e
rnldvrgl <delacruz.ronaldvergel@gmail.com>
2025-04-16 00:02:05 +0800

    chore: refactor HomePage with useFetch

--- src/components/ListingList.jsx
+++ src/components/ListingList.jsx
@@ -2,7 +2,7 @@ import ListingCard from "./ListingCard";
 const ListingList = ({listings}) => {
   return (
       <div className='flex flex-wrap justify-center gap-4'>
-      {listings.length > 0 ? (
+      {listings?.length > 0 ? (
         listings.map((listing) => (
           <ListingCard key={listing.id} listing={listing} />
         ))
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,66 +1,41 @@
-'use client';
+import { useState } from 'react';
 
-import api from '@/api';
 import ListingFilters from '@/components/ListingFilters';
 import ListingList from '@/components/ListingList';
 import { Separator, Spinner } from '@/components/ui';
-import { useEffect, useRef, useState } from 'react';
+import useFetch from '@/hooks/useFetch';
+
 const HomePage = () => {
-  const [listings, setListings] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState(false);
   const [filters, setFilters] = useState({
     dates: undefined,
     guests: 0,
     search: '',
   });
 
-  const abortController = useRef(null);
+  const {
+    data: listings,
+    error,
+    isLoading,
+  } = useFetch('/api/listings', { params: filters });
 
-  useEffect(() => {
-    const fetchListings = async () => {
-      setIsLoading(true);
-      setError(null);
-
-      try {
-        const response = await api.get('/api/listings', { params: filters, signal: abortController.current?.signal });
-
-        setListings(response.data);
-      } catch (error) {
-        if(axios.isCancel(error)){
-          return;
-        }
-
-        setError('Something went wrong. Please try again later.');
-      } finally {
-        setIsLoading(false);
-      }
-    };
-    fetchListings();
-
-    // clean up
-    return () => {
-      abortController.current?.abort();
-    }
-  }, [filters]);
+  const handleFilters = (filters) => {
+    setFilters(filters);
+  };
 
   const renderListingList = () => {
     if (isLoading) {
+      return (
         <div className='flex justify-center'>
           <Spinner size='sm' />
         </div>
+      );
     }
 
     if (error) {
       return <div className='text-center'>{error}</div>;
     }
 
-    
     return <ListingList listings={listings} />;
-  }
-
-  const handleFilters = (filters) => {
-    setFilters(filters);
   };
 
   return (

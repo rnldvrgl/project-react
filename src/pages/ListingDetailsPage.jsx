f494e8870dcb78ddbd31abe1fc06d5f78ca426bb
rnldvrgl <delacruz.ronaldvergel@gmail.com>
2025-04-22 23:10:35 +0800

    chore: refactor the ListingDetailsPage with useFetch

--- src/pages/ListingDetailsPage.jsx
+++ src/pages/ListingDetailsPage.jsx
@@ -1,44 +1,17 @@
-import { useEffect, useRef, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
-import api from '@/api';
 import ListingDetailsCard from '@/components/ListingDetailsCard';
 import { Spinner } from '@/components/ui';
+import useFetch from '@/hooks/useFetch';
 
 const ListingDetailsPage = () => {
   const { listingId } = useParams();
 
-  const [listing, setListing] = useState();
-  const [error, setError] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
-
-  const abortController = useRef(null);
-
-  useEffect(() => {
-    const fetchListing = async () => {
-      setIsLoading(true);
-      setError(null);
-
-      abortController.current = new AbortController();
-
-      try {
-        const response = await api.get(`/api/listings/${listingId}`, {
-          signal: abortController.current?.signal,
-        });
-        setListing(response.data);
-      } catch {
-        setError('Something went wrong. Please try again later.');
-      } finally {
-        setIsLoading(false);
-      }
-    };
-
-    fetchListing();
-
-    return () => {
-      abortController.current?.abort();
-    };
-  }, [listingId]);
+  const {
+    data: listing,
+    error,
+    isLoading,
+  } = useFetch(`/api/listings/${listingId}`);
 
   const renderListing = () => {
     if (isLoading) {

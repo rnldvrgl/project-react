6c900eebc2ddf21ebe0c00d93c2a4644727e5519
rnldvrgl <delacruz.ronaldvergel@gmail.com>
2025-05-28 18:09:47 +0800

    feat: refactor the HomePage with listingsSlice

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,16 @@
-import { useCallback, useMemo, useState } from 'react';
+import { useCallback, useEffect, useMemo, useState } from 'react';
 
 import DataRenderer from '@/components/DataRenderer';
 import ListingFilters from '@/components/ListingFilters';
 import ListingList from '@/components/ListingList';
 import { Separator } from '@/components/ui';
-import useFetch from '@/hooks/useFetch';
+import { fetchListings } from '@/state/listings/listingSlice';
+import { useDispatch, useSelector } from 'react-redux';
 
 const HomePage = () => {
+  const { listings, error, status } = useSelector((state) => state.listings);
+  const dispatch = useDispatch();
+
   const [filters, setFilters] = useState({
     dates: undefined,
     guests: 0,
@@ -17,11 +21,13 @@ const HomePage = () => {
     params: filters
   }), [filters])
 
-  const {
-    data: listings,
-    error,
-    isLoading,
-  } = useFetch('/api/listings', fetchOptions);
+  useEffect(() => {
+    const request = dispatch(fetchListings(fetchOptions));
+
+    return () => {
+      request.abort();
+    };
+  }, [dispatch, fetchOptions]);
 
   const handleFilters = useCallback((filters) => {
     setFilters(filters)
@@ -34,7 +40,7 @@ const HomePage = () => {
         <ListingFilters onChange={handleFilters} />
         <Separator className='my-4' />
       </div>
-      <DataRenderer error={error} isLoading={isLoading}>
+      <DataRenderer error={error} isLoading={status === 'loading'}>
         <ListingList listings={listings} />
       </DataRenderer>
     </div>

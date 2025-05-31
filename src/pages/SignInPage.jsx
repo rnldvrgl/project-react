126df715534779ca30e0cf3bb15d2e5cab85669f
rnldvrgl <delacruz.ronaldvergel@gmail.com>
2025-05-31 11:04:39 +0800

    chore: create the `SignInPage` component

---
+++ src/pages/SignInPage.jsx
@@ -0,0 +1,9 @@
+const SignInPage = () => {
+  return (
+    <div className='container flex h-screen items-center justify-center py-4'>
+      Sign in form goes here!
+    </div>
+  );
+};
+
+export default SignInPage;

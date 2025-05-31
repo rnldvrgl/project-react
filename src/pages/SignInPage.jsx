86e00f1bac6c5a6bb43f72f42ddfdf260a845e19
rnldvrgl <delacruz.ronaldvergel@gmail.com>
2025-05-31 14:17:38 +0800

    chore: add SignInForm to SignInPage

--- src/pages/SignInPage.jsx
+++ src/pages/SignInPage.jsx
@@ -1,7 +1,9 @@
+import SignInForm from '@/components/SignInForm';
+
 const SignInPage = () => {
   return (
     <div className='container flex h-screen items-center justify-center py-4'>
-      Sign in form goes here!
+      <SignInForm />
     </div>
   );
 };

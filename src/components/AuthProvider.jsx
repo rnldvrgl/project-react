bce631b6cf23eaa44fd0cd01328ef726b24f6d96
rnldvrgl <delacruz.ronaldvergel@gmail.com>
2025-05-31 21:23:34 +0800

    feat: refresh the token when expired

--- src/components/AuthProvider.jsx
+++ src/components/AuthProvider.jsx
@@ -38,9 +38,10 @@ const AuthProvider = ({ children }) => {
 
   useLayoutEffect(() => {
     const authInterceptor = api.interceptors.request.use((config) => {
-      config.headers.Authorization = token
-        ? `Bearer ${token}`
-        : config.headers.Authorization;
+      config.headers.Authorization =
+        !config._retry && token
+          ? `Bearer ${token}`
+          : config.headers.Authorization;
       return config;
     });
 
@@ -48,6 +49,39 @@ const AuthProvider = ({ children }) => {
       api.interceptors.request.eject(authInterceptor);
     };
   }, [token]);
+
+  useLayoutEffect(() => {
+    const refreshInterceptor = api.interceptors.response.use(
+      (response) => response,
+      async (error) => {
+        const originalRequest = error.config;
+
+        if (
+          error.response.status === 403 &&
+          error.response.data.message === 'Unauthorized'
+        ) {
+          try {
+            const response = await api.get('/api/refreshToken');
+
+            setToken(response.data.accessToken);
+
+            originalRequest.headers.Authorization = `Bearer ${response.data.accessToken}`;
+            originalRequest._retry = true;
+
+            return api(originalRequest);
+          } catch {
+            setToken(null);
+          }
+        }
+
+        return Promise.reject(error);
+      },
+    );
+
+    return () => {
+      api.interceptors.response.eject(refreshInterceptor);
+    };
+  }, []);
 
   return (
     <AuthContext.Provider value={{ token, setToken }}>
